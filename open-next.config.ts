e91494180fd800d6120d291a42ce325c961ff9cb
thamarai <Thamaraikannan.B@kryptosinfosys.com>
2026-08-25 14:31:10 +0530

    fix: proper openNext cloudflare config

---
+++ open-next.config.ts
@@ -0,0 +1,9 @@
+import type { OpenNextConfig } from 'open-next/types/open-next.js';
+
+const config = {
+  buildDir: '.open-next',
+  compress: false,
+  imageOptimization: false,
+} satisfies OpenNextConfig;
+
+export default config;
